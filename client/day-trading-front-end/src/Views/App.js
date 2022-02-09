51939e3a66f72fbe766011058ecb4123eacd4f66
Adam Kwan <adam876k@gmail.com>
2022-02-09 14:01:49 -0800

    Create Login page and clean up repo

---
+++ client/day-trading-front-end/src/Views/App.js
@@ -0,0 +1,13 @@
+import '../css/App.css';
+import Login from './Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
--- client/day-trading-front-end/src/index.js
+++ client/day-trading-front-end/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+import App from './Views/App';
+import reportWebVitals from './test/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
