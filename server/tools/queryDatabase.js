348b7cf35d3a41ea262b7abb1c6d420b76cd9909
Nolan <kurylon@telus.net>
2022-02-20 18:39:45 -0800

    fixed database pooling issue

--- server/tools/queryDatabase.js
+++ server/tools/queryDatabase.js
@@ -2,14 +2,14 @@
 require('dotenv').config()
 const Pool = require('pg').Pool
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
+
+})
 
 module.exports = async (text, values, cb) => {
 
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: false
-  
-  })
   const client = await pool.connect()
   .catch((err) => {
     console.log("Failure to connect to pool: " + err)
