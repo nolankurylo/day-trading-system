1ec9ec84e729716aeb8457137c2cb2f7a63d75d9
liamfranke <liamfrankework@gmail.com>
2022-04-13 13:45:08 -0700

    Full Stack Requests - For 468 Demo

--- server/routes/get.js
+++ server/routes/get.js
@@ -24,7 +24,7 @@ none
 */
 router.get("/test",  (req, res) => {
 
-  query("SELECT * from transactions where console != 'console'",[],async (err, result) => {
+  query("SELECT * from users where role != 'console'",[],async (err, result) => {
     if (err) return dbFail.failSafe(err, res);
     return res.send(result);
   })
